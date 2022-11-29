8e52e046b1c7a810ca8791fe0e20a70964cad7e9
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-11-29 16:24:08 +0400

    add reusable view LogoAndSearchView

---
+++ CookBook/HomePage/HomePage.swift
@@ -0,0 +1,19 @@
+//
+//  HomePage.swift
+//  CookBook
+//
+//  Created by Veronica Rudiuk on 29.11.22.
+//
+
+import UIKit
+
+class HomePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
